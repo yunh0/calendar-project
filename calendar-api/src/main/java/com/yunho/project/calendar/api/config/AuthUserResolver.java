a8b39c03be18ea16b5e287f1e91c64e672ec2064
YunhoNam <nam100400@naver.com>
2024-07-11 18:03:26 +0900

    Add - Argument Resolver AuthUser 추가

---
+++ calendar-api/src/main/java/com/yunho/project/calendar/api/config/AuthUserResolver.java
@@ -0,0 +1,29 @@
+package com.yunho.project.calendar.api.config;
+
+import com.yunho.project.calendar.api.dto.AuthUser;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import static com.yunho.project.calendar.api.service.LoginService.LOGIN_SESSION_KEY;
+
+public class AuthUserResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return AuthUser.class.isAssignableFrom(parameter.getParameterType());
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        final Long userId = (Long) webRequest.getAttribute(LOGIN_SESSION_KEY, WebRequest.SCOPE_SESSION);
+        if (userId != null) {
+            return AuthUser.of(userId);
+        } else {
+            throw new RuntimeException("bad request. no session");
+        }
+    }
+}
