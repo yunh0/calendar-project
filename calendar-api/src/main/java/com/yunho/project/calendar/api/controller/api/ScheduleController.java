0130e28b3817cf8fab23a5e0a18d8216350b0ae3
YunhoNam <nam100400@naver.com>
2024-07-11 21:52:16 +0900

    Add - Notification 생성 구현

--- calendar-api/src/main/java/com/yunho/project/calendar/api/controller/api/ScheduleController.java
+++ calendar-api/src/main/java/com/yunho/project/calendar/api/controller/api/ScheduleController.java
@@ -2,8 +2,10 @@ package com.yunho.project.calendar.api.controller.api;
 
 import com.yunho.project.calendar.api.dto.AuthUser;
 import com.yunho.project.calendar.api.dto.EventCreateReq;
+import com.yunho.project.calendar.api.dto.NotificationCreateReq;
 import com.yunho.project.calendar.api.dto.TaskCreateReq;
 import com.yunho.project.calendar.api.service.EventService;
+import com.yunho.project.calendar.api.service.NotificationService;
 import com.yunho.project.calendar.api.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,7 @@ public class ScheduleController {
 
     private final TaskService taskService;
     private final EventService eventService;
+    private final NotificationService notificationService;
 
     @PostMapping("/tasks")
     public ResponseEntity<Void> createTask(@RequestBody TaskCreateReq taskCreateReq,
@@ -31,6 +34,13 @@ public class ScheduleController {
     public ResponseEntity<Void> createTask(@RequestBody EventCreateReq eventCreateReq,
                                             AuthUser authUser) {
         eventService.create(eventCreateReq, authUser);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/notifications")
+    public ResponseEntity<Void> createTask(
+            @RequestBody NotificationCreateReq notificationCreateReq, AuthUser authUser) {
+        notificationService.create(notificationCreateReq, authUser);
         return ResponseEntity.ok().build();
     }
 }
