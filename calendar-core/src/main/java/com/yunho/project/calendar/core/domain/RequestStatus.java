50dbbff8129c246cf27348e8e96856321a5de034
YunhoNam <nam100400@naver.com>
2024-07-11 12:05:57 +0900

    Add - 도메인 pojo 객체 생성

---
+++ calendar-core/src/main/java/com/yunho/project/calendar/core/domain/RequestStatus.java
@@ -0,0 +1,5 @@
+package com.yunho.project.calendar.core.domain;
+
+public enum RequestStatus {
+    REQUESTED, ACCEPTED, REJECTED
+}
---
+++ calendar-core/src/main/java/com/yunho/project/calendar/core/domain/Task.java
@@ -0,0 +1,19 @@
+package com.yunho.project.calendar.core.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class Task {
+
+    private Long id;
+    private LocalDateTime taskAt;
+    private String title;
+    private String description;
+    private User writer;
+    private LocalDate createdAt;
+}
