f02f327898d4b1758143acb7cbf660d2741ca713
YunhoNam <nam100400@naver.com>
2024-07-14 17:44:58 +0900

    Update - 메일 날짜 포맷팅
    재귀를 통해서 구현

--- calendar-api/src/main/java/com/yunho/project/calendar/api/dto/EngagementEmailStuff.java
+++ calendar-api/src/main/java/com/yunho/project/calendar/api/dto/EngagementEmailStuff.java
@@ -1,18 +1,45 @@
 package com.yunho.project.calendar.api.dto;
 
 import com.yunho.project.calendar.core.util.Period;
+import org.springframework.data.util.Pair;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.function.Predicate;
 
 public class EngagementEmailStuff {
     private static final String engagementUpdateApi = "http://localhost:8080/events/engagements/";
+    public static final String MAIL_TIME_FORMAT = "yyyy년 MM월 dd일(E) a hh시 mm분";
+    public static final List<Pair<String, Predicate<Period>>> endAtFormatParts = Arrays.asList(
+            Pair.of("yyyy년 ", period -> period.getEndAt().getYear() == period.getStartAt().getYear()),
+            Pair.of("MM월 ", period -> period.getEndAt().getMonth() == period.getStartAt().getMonth()),
+            Pair.of("dd일(E) ", period -> period.getEndAt().getDayOfMonth() == period.getStartAt().getDayOfMonth())
+    );
+
+    public static String getEndAtFormat(Period period,
+                                        String format,
+                                        List<Pair<String, Predicate<Period>>> remainEndAtFormatParts) {
+        if (remainEndAtFormatParts.isEmpty()) {
+            return format;
+        } else if (remainEndAtFormatParts.get(0).getSecond().test(period)) {
+            return getEndAtFormat(
+                    period,
+                    format.replace(remainEndAtFormatParts.get(0).getFirst(), ""),
+                    remainEndAtFormatParts.subList(1, remainEndAtFormatParts.size()));
+        } else {
+            return format;
+        }
+    }
+
     private final Long engagementId;
     private final String toEmail;
     private final List<String> attendeeEmails;
     private final String title;
     private final Period period;
+    private final String periodStr;
 
     public EngagementEmailStuff(Long engagementId,
                                 String toEmail,
@@ -24,6 +51,13 @@ public class EngagementEmailStuff {
         this.attendeeEmails = attendeeEmails;
         this.title = title;
         this.period = period;
+        this.periodStr = getPeriodStrRecursive();
+    }
+
+    private String getPeriodStrRecursive() {
+        final String endAtFormat = getEndAtFormat(period, MAIL_TIME_FORMAT, endAtFormatParts);
+        return period.getStartAt().format(DateTimeFormatter.ofPattern(MAIL_TIME_FORMAT)) + " - "
+                + period.getEndAt().format(DateTimeFormatter.ofPattern(endAtFormat));
     }
 
     public String getToEmail() {
@@ -35,7 +69,7 @@ public class EngagementEmailStuff {
                 .append("[초대장] ")
                 .append(title)
                 .append(" - ")
-                .append(period.toString())
+                .append(periodStr)
                 .append("(")
                 .append(toEmail)
                 .append(")")
@@ -46,7 +80,7 @@ public class EngagementEmailStuff {
         final Map<String, Object> props = new HashMap<>();
         props.put("title", title);
         props.put("calendar", toEmail);
-        props.put("period", period.toString());
+        props.put("period", periodStr);
         props.put("attendees", attendeeEmails);
         props.put("acceptUrl", engagementUpdateApi + engagementId + "?type=ACCEPT");
         props.put("rejectUrl", engagementUpdateApi + engagementId + "?type=REJECT");
