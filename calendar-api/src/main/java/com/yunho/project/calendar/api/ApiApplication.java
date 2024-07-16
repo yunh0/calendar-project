2128aa292025f29ff776c5aafde31bd9b6cce93d
YunhoNam <nam100400@naver.com>
2024-07-16 15:08:35 +0900

    Delete - 프로젝트 생성 시 추가한 테스트 코드 삭제

--- calendar-api/src/main/java/com/yunho/project/calendar/api/ApiApplication.java
+++ calendar-api/src/main/java/com/yunho/project/calendar/api/ApiApplication.java
@@ -1,35 +1,11 @@
 package com.yunho.project.calendar.api;
 
-import com.yunho.project.calendar.core.SimpleEntity;
-import com.yunho.project.calendar.core.SimpleEntityRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
-@RestController
 @SpringBootApplication(scanBasePackages = "com.yunho.project")
 public class ApiApplication {
-
-    private final SimpleEntityRepository repository;
-
-    public ApiApplication(SimpleEntityRepository repository) {
-        this.repository = repository;
-    }
-
-    @GetMapping
-    public List<SimpleEntity> findAll(){
-        return repository.findAll();
-    }
-
-    @PostMapping("/save")
-    public SimpleEntity saveOne(){
-        return repository.save(new SimpleEntity());
-    }
-
 
     public static void main(String[] args) {
 
--- calendar-core/src/main/java/com/yunho/project/calendar/core/SimpleEntityRepository.java
+++
@@ -1,6 +0,0 @@
-package com.yunho.project.calendar.core;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SimpleEntityRepository extends JpaRepository<SimpleEntity, Long> {
-}
